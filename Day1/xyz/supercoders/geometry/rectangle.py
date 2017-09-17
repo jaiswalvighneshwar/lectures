dae31b2595daf03c17161b8a7bff8e4351b0eabd
jaiswalvighneshwar <jaiswal.vighneshwar@gmail.com>
2017-09-17 16:35:32 +0530

    added files for day

---
+++ Day1/xyz/supercoders/geometry/rectangle.py
@@ -0,0 +1,12 @@
+from shape_stats import Shape
+class Rectangle(Shape):
+	
+	def __init__(self,length,breadth):
+		self.length = length
+		self.breadth = breadth 
+
+	def area(self):
+		return self.length * self.breadth	
+	
+	def perimeter(self):
+		return 2 * (self.length + self.breadth)
---
+++ Day1/xyz/supercoders/geometry/square.py
@@ -0,0 +1,14 @@
+from shape_stats import Shape
+
+class Square(Shape):
+
+	def __init__(self,side):
+	  self.side = side
+
+
+	def area(self):
+		return self.side * self.side
+
+	def perimeter(self):
+		return 4 * self.side
+
